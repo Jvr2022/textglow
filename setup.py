3fa236eee34f88cc124e448f172a8d3988dc8ff2
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-09-19 17:25:46 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='textglow',
+    version='0.1',
+    description='A Python package to add glow effects to text in terminal-based applications.',
+    author='JVR2022',
+    author_email='joshuavanrijswijk@outlook.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
